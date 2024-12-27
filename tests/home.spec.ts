d22c650de751d157a1afcf195ecd9ea9132999ca
Brian Ho <brian.ho@jucyworld.com>
2024-12-27 16:29:46 +1300

    wrote a test using a locator to get 'img' tag in an 'a' tag

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,8 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test('Open home page and verify title', async ({ page }) => {
-  // test steps
   await page.goto('https://www.fifa.com/en');
 
   await expect(page).toHaveTitle('FIFA | The Home of Football');
+});
+
+test('Verify logo is visible and has the alt title', async ({ page }) => {
+  await page.goto('https://www.fifa.com/en');
+
+  const logo = page.locator('[href="/en/home"] img');
+  await expect(logo).toBeVisible();
+  await expect(logo).toHaveAttribute('title', 'FIFA');
+  await expect(logo).toHaveAttribute('alt', 'FIFA');
 });
